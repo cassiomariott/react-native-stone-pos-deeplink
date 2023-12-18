0d989493900588af248f93c290260c92b8eaf3e8
Cassio Mariott <cassiomariott@gmail.com>
2023-12-18 18:39:51 -0300

    version "0.1.4"
    Implemented cancel transaction

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,51 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-stone-pos-deeplink';
+import { StyleSheet, View, Text, Pressable } from 'react-native';
+import { cancelTransaction, makeTransaction, reprinterTransaction } from 'react-native-stone-pos-deeplink';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
 
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
+  const onMakeTransaction = () => {
+    makeTransaction({      
+      amount: "500",
+      amountOfEditable: "0",
+      installmentCount: "1",
+      typeOfTransaction: "DEBIT", /* DEBIT, CREDIT, VOUCHER, INSTANT_PAYMENT e PIX */
+      typeOfInstallment: "MERCHANT" /* MERCHANT (parcelado sem juros), ISSUER (parcelado com juros) e NONE (à vista) */
+    })
+  }
+
+  const onReprinterTransaction = () => {
+    reprinterTransaction({      
+      showw_feedback_screen: "true",
+      type_customer: 'MERCHANT', /* CLIENT (cliente), MERCHANT (lojista) */
+      atk: "000000000000"
+    })
+  }
+
+  const onCancelTransaction = () => {
+    cancelTransaction({      
+      editable_amount: "false",
+      amount: "500",
+      atk: "000000000000"
+    })
+  }
 
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+
+      <Pressable onPress={() => { onMakeTransaction() }}>
+        <Text>Make Transaction</Text>
+      </Pressable>
+
+      <Pressable onPress={() => { onReprinterTransaction() }}>
+        <Text>Reprinter Transaction</Text>
+      </Pressable>
+      
+      <Pressable onPress={() => { onCancelTransaction() }}>
+        <Text>Cancel Transaction</Text>
+      </Pressable>
+
     </View>
   );
 }
